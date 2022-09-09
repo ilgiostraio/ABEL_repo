75406028af80abe7212d51cdf1a46f8f82d8abb8
Lorenzo Cominelli <107864420+ilgiostraio@users.noreply.github.com>
2022-09-09 11:19:24 +0200

    Delete emotiva directory

--- emotiva/simple.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-from emautionanalysis import StaticModel
-
-img_path = "C:\\Users\\FACETeam\\Desktop\\EMOTIVA\\rob.jpg"
-img = cv2.imread(img_path)
-
-model = StaticModel(model_name="cnn_model")
-print(model.predict_emotions(img=img))
